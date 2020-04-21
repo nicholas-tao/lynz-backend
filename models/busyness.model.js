dfa88f550310b60c1539f1d24a70f7c53f1e0d1e
nicholas-tao <tao.nicholas1@gmail.com>
2020-04-21 17:03:57 -0400

    initial commit

---
+++ models/busyness.model.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const busynessSchema = new Schema(
+  {
+    storeAddress: { type: String, required: true },
+    storeName: { type: String, required: true },
+    busyness: { type: String, required: true },
+    time: { type: Date, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Busyness = mongoose.model("Busyness", busynessSchema);
+
+module.exports = Busyness;
