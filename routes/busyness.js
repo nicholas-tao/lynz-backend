4559e5a81318fc74c62aa1c6748b8fcaee06243c
nicholas-tao <jacksana1@gmail.com>
2020-04-23 00:04:34 -0400

    integrated google maps api

--- routes/busyness.js
+++ routes/busyness.js
@@ -1,5 +1,100 @@
+//AIzaSyDssQoht_O4WR5jLRT0cLUtenY7eL3Answ
+//43.878960,-79.413383
+
+require("dotenv").config();
+
 const router = require("express").Router();
 let Busyness = require("../models/busyness.model");
+
+const axios = require("axios");
+
+var names = [];
+var address = [];
+var rating = [];
+var storeSize = [];
+var storeId = [];
+
+const API_KEY = process.env.GOOGLE_MAPS_API_KEY;
+
+//this stuff to get from frontend
+var longitude = 43.87896;
+var latitude = -79.413383;
+var radius = 5000; //in metres
+
+var link1 =
+  "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" +
+  longitude +
+  "," +
+  latitude +
+  "&radius=" +
+  radius +
+  "&type=grocery_or_supermarket&fields=name,formatted_address,user_ratings_total,place_id&key=" +
+  API_KEY;
+
+router.get("/getstores", (request, response) => {
+  var data = [];
+  axios
+    .get(link1)
+    .then((getResponse) => {
+      console.log("GET Response");
+      data = getResponse.data;
+      response.send(data);
+      console.log(data.results[0].name);
+
+      for (var i = 0; i < data.results.length; i++) {
+        names[i] = data.results[i].name;
+        address[i] = data.results[i].vicinity;
+        rating[i] = data.results[i].rating;
+        storeSize[i] = data.results[i].user_ratings_total;
+        storeId[i] = data.results[i].id;
+      }
+      sortSizes();
+      display();
+    })
+    .catch(function (error) {
+      console.log("Error occured");
+    });
+});
+
+function display() {
+  for (var i = 0; i < names.length; i++) {
+    if (names[i] == null) {
+      break;
+    }
+    console.log(names[i]);
+  }
+}
+
+// sort stores by size
+function sortSizes() {
+  var swapp;
+  var n = storeSize.length - 1;
+  do {
+    swapp = false;
+    for (var i = 0; i < n; i++) {
+      if (storeSize[i] < storeSize[i + 1]) {
+        var temp = storeSize[i];
+        storeSize[i] = storeSize[i + 1];
+        storeSize[i + 1] = temp;
+
+        var temp2 = names[i];
+        names[i] = names[i + 1];
+        names[i + 1] = temp2;
+
+        var temp3 = rating[i];
+        rating[i] = rating[i + 1];
+        rating[i + 1] = temp3;
+
+        var temp4 = address[i];
+        address[i] = address[i + 1];
+        address[i + 1] = temp4;
+
+        swapp = true;
+      }
+    }
+    n--;
+  } while (swapp);
+}
 
 router.route("/").get((req, res) => {
   Busyness.find()
@@ -34,6 +129,7 @@ router.route("/:id").delete((req, res) => {
     .then(() => res.json("Busyness deleted."))
     .catch((err) => res.status(400).json("Error: " + err));
 });
+/*
 router.route("/update/:id").post((req, res) => {
   Busyness.findById(req.params.id)
     .then((busyness) => {
@@ -48,5 +144,6 @@ router.route("/update/:id").post((req, res) => {
     })
     .catch((err) => res.status(400).json("Error: " + err));
 });
+*/
 
 module.exports = router;
